2e77ac3f9e88ad4332378bdc0eceadb0b9b6a12f
nhungntph12785 <65637961+nhungntph12785@users.noreply.github.com>
2022-03-05 16:14:23 +0700

    Nhung

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -29,10 +29,13 @@
         <td> {{$item->name}} </td>
         <td> {{$item->email}} </td>
         <td><img src="{{asset($item->avatar)}}" alt="" width="100"></td>
+        <td> {{$item->phone}} </td>
+        <td> {{$item->address}} </td>
+        <td> {{$item->description}} </td>
        
-        {{-- <td> <button type="button" class="btn btn-primary"><a style="color:white;  text-decoration: none;" href="{{ route('role.edit', ['id'=>$item->id]) }}"> Edit</a></button>
+        <td> <button type="button" class="btn btn-primary"><a style="color:white;  text-decoration: none;" href="{{ route('role.edit', ['id'=>$item->id]) }}"> Edit</a></button>
             <button type="button" class="btn btn-danger"><a style="color:white;  text-decoration: none;" onclick="return confirm('Bạn có chắc muốn User')"  href="{{ route('role.remove', ['id'=>$item->id]) }}"> Delete</a></button>
-        </td> --}}
+        </td>
       </tr>
       @endforeach
     </tbody>
