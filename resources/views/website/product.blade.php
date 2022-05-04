14b053ba63da60727551523e32275154add4fd57
nongdung11 <emgals08@gmail.com>
2022-05-04 22:10:54 +0700

    fix view
    fix view

--- resources/views/website/product.blade.php
+++ resources/views/website/product.blade.php
@@ -5,14 +5,6 @@
 
 <head>
 
-        p.card-text {
-            -webkit-line-clamp: 3;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-        }
-    </style>
     <title>Bệnh Viện Laptop 51</title>
     <meta name="keywords" content="" />
     <meta name="description" content="" />
@@ -117,7 +109,6 @@ if (!function_exists('currency_format')) {
                                 <!-- shop-pagination end -->
                             </div>
                         </div>
-
                         <div class="col-lg-3 order-lg-1 order-2">
                             <!-- widget-search -->
                             <aside class="widget-search mb-30">
@@ -169,108 +160,10 @@ if (!function_exists('currency_format')) {
             </div>
             <!-- SHOP SECTION END -->
         </div>
-        
-                <div class="col-lg-3 d-flex col-sm-6 p-2">
-                    <div class="card flex-fill">
-                        {{-- @foreach ($images as $image)
-                        @if ($image->product_id == $item->id)
-                        <a href="/san-pham/{{ $item->slug }}"><img src="{{ asset($image->path) }}" class="card-img-top"
-                                alt="{{ asset($image->path) }}"></a>
-                        @break
-
-                        ;
-                        @endif
-                        @endforeach --}}
-                        <div class="card-body">
-                            <p class="card-title h6 fw-bold p-0">{{ $item->name }}</p>
-                            <p class="card-text h6 small p-0">{{ $item->desc_short }}
-                            </p>
-                            <h6 class="bg-warning font-weight-bold mt-3 mb-0 mx-auto p-1 rounded-pill text-center text-danger"
-                                style="width: 180px;">{{ currency_format($item->price) }}</h6>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            @foreach ($ComputerCompany as $comP)
-            <div class="pt-2 mx-0 my-0">
-                <div class="row bg-white shadow mx-0">
-                    <div class="col-auto justify-content-start bg-warning pt-2">
-                        <h4 class="text-dark font-weight-bold">
-                            LAPTOP {{ $comP->company_name }}
-                        </h4>
-                    </div>
-                    <div class="col mx-0">
-                        <ul class="nav justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link text-dark" href="cua-hang/{{ $comP->id }}">Xem tất cả</a>
-                            </li>
-                            <!-- <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-        </li> -->
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row p-2">
-                <?php
-                    $i = 0;
-                    $y = 1;
-                    ?>
-
-                @foreach ($products as $product)
-                @if ($product->companyComputer_id == $comP->id)
-                <?php
-                            $i = $i + 1;
-                            $y += $y;
-                            if (!function_exists('currency_format')) {
-                                function currency_format($product, $suffix = ' VNĐ')
-                                {
-                                    if (!empty($product)) {
-                                        return number_format($product, 0, ',', '.') . "{$suffix}";
-                                    }
-                                }
-                            }
-                            
-                            ?>
-                <div class="col-lg-3 d-flex col-sm-6 p-2">
-                    <div class="card flex-fill">
-                        @foreach ($images as $image)
-                        @if ($image->product_id == $product->id)
-                        <a href="/san-pham/{{ $product->slug }}"><img src="{{ asset($image->path) }}"
-                                class="card-img-top" alt="{{ asset($image->path) }}"></a>
-                        @break
-
-                        ;
-                        @endif
-                        @endforeach,
-                        <div class="card-body">
-                            <p class="card-title h6 fw-bold p-0">{{ $product->name }}</p>
-                            <p class="card-text h6 small p-0">{{ $product->desc_short }}
-                            </p>
-                            <h6 class="bg-warning font-weight-bold mt-3 mb-0 mx-auto p-1 rounded-pill text-center text-danger"
-                                style="width: 180px;">{{ currency_format($product->price) }}</h6>
-                        </div>
-                    </div>
-                </div>
-                @if ($i === 4)
-                @break
-                @endif
-                @endif
-                @endforeach
-            </div>
-            @endforeach
-        </div>
-    </div>
         @include('layout_client.footer')
         @include('layout_client.script')
+
+
 
 </body>
 
